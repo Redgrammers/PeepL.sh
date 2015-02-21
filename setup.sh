3b096e8b03741dfc52bca0f7936e95437bbabf9a
Raumaan Kidwai <raumaankidwai@gmail.com>
2015-02-21 12:59:43 -0500

    Setup file for PeepL

--- setup.sh
+++ setup.sh
@@ -5,3 +5,11 @@ setup () {
 	source cleanup.sh
 }
 sudo -p "Administrator Password: " setup
+if [ "$?" = "1" ]; then exit 1; fi
+cd /usr/local
+which -s sha256sum
+if [ "$?" = "0" ]; then
+	echo "echo $1 | sha256sum" >> compat/shasum.sh
+else
+	echo "echo $1 | shasum -a 256" >> compat/shasum.sh
+fi
