b4618c369948d239995a79a4a2dfd3cae87b5b59
Raumaan Kidwai <raumaankidwai@gmail.com>
2015-02-21 19:26:21 -0500

    Setup script for PeepL

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo -p "Administrator Password: " sudo/setup.sh
 if [ "$?" = "1" ]; then exit 1; fi
 cd /usr/local
+rm -r README.md*
 which -s sha256sum
 if [ "$?" = "0" ]; then
 	echo "echo $1 | sha256sum" >> compat/shasum
